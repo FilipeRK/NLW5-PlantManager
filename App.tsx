ba600c295060e48d300b104fc9e0f753412433bb
Filipe Kohls <filiperenank@gmail.com>
2021-04-21 01:03:50 -0300

    Criadas novas páginas, criada a navegação entre as paginas e componentizado o botão

--- App.tsx
+++ App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import { Welcome } from './src/pages/welcome';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
+import Routes from './src/routes';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  //Enquanto a fonte não for carregada, dá uma segurada com a splash screen
+  if (!fontsLoaded)
+    return <AppLoading />
+
   return (
-    <Welcome />
+    <Routes />
   )
 }
